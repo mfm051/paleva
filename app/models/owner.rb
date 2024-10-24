63e5f11f9caa4e0e05e98c34ef01f27353ad2364
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-10-24 14:18:45 -0300

    Simplifica mensagens de erro para dono

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -12,19 +12,25 @@ class Owner < ApplicationRecord
   private
 
   def cpf_must_have_11_chars
-    unless cpf.present? && cpf.length == 11
+    return unless cpf.present?
+
+    unless cpf.length == 11
       errors.add(:cpf, 'deve ter 11 caracteres')
     end
   end
 
   def cpf_must_be_numeric
-    unless cpf.present? && cpf.chars.all? { |char| ('0'..'9').include? char }
+    return unless cpf.present?
+
+    unless cpf.chars.all? { |char| ('0'..'9').include? char }
       errors.add(:cpf, 'deve ser composto apenas por números')
     end
   end
 
   def cpf_must_be_valid
-    unless cpf.present? && CPF.valid?(cpf)
+    return unless cpf.present?
+
+    unless CPF.valid?(cpf)
       errors.add(:cpf, 'não é válido')
     end
   end
