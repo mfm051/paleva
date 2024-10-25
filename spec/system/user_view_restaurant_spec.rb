ba2d9167080cb7da4925370faeeddbfb2ff3291b
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-10-25 11:29:05 -0300

    Adiciona informações de pratos e bebidas à tela inicial

--- spec/system/user_view_restaurant_spec.rb
+++ spec/system/user_view_restaurant_spec.rb
@@ -10,4 +10,70 @@ describe 'Usuário vê seu restaurante na tela inicial' do
 
     expect(page).to have_content 'Para continuar, registre seu estabelecimento'
   end
+
+  it 'e vê pratos cadastrados' do
+    owner = Owner.create!(cpf: '34423090007', name: 'Paula', surname: 'Groselha', email: 'paula@email.com',
+                              password: '123456789012')
+    restaurant = owner.create_restaurant!(brand_name: 'A Figueira Rubista', corporate_name: 'Figueira Rubista LTDA',
+                            cnpj: '25401196000157', full_address: 'Rua das Flores, 10', phone: '1525017617',
+                            email: 'afigueira@email.com')
+    restaurant.dishes.create!(name: 'Provoleta de Cabra grelhada', description: 'Entrada')
+    restaurant.dishes.create!(name: 'Salada de Palmito e Agrião', description: 'Salada')
+
+    login_as(owner)
+    visit root_path
+
+    expect(page).to have_content 'Pratos'
+    expect(page).to have_content 'Provoleta de Cabra grelhada'
+    expect(page).to have_content 'Entrada'
+    expect(page).to have_content 'Salada de Palmito e Agrião'
+    expect(page).to have_content 'Salada'
+  end
+
+  it 'e não há pratos cadastrados' do
+    owner = Owner.create!(cpf: '34423090007', name: 'Paula', surname: 'Groselha', email: 'paula@email.com',
+                              password: '123456789012')
+    owner.create_restaurant!(brand_name: 'A Figueira Rubista', corporate_name: 'Figueira Rubista LTDA',
+                            cnpj: '25401196000157', full_address: 'Rua das Flores, 10', phone: '1525017617',
+                            email: 'afigueira@email.com')
+
+    login_as(owner)
+    visit root_path
+
+    expect(page).to have_content 'Ainda não há pratos cadastrados'
+  end
+
+  it 'e vê bebidas cadastrados' do
+    owner = Owner.create!(cpf: '34423090007', name: 'Paula', surname: 'Groselha', email: 'paula@email.com',
+                              password: '123456789012')
+    restaurant = owner.create_restaurant!(brand_name: 'A Figueira Rubista', corporate_name: 'Figueira Rubista LTDA',
+                            cnpj: '25401196000157', full_address: 'Rua das Flores, 10', phone: '1525017617',
+                            email: 'afigueira@email.com')
+    drink1 = restaurant.drinks.create!(name: 'Coca-cola', alcoholic: false, description: 'Só uma Coca-cola')
+    drink2 = restaurant.drinks.create!(name: 'Mistério', alcoholic: true, description: 'Vermouth Dourado infusionado'\
+                                      ' com grãos de café, Bacardi 8 anos, licor Grand Marnier e Bitter Aromatic.')
+
+    login_as(owner)
+    visit root_path
+
+    expect(page).to have_content 'Bebidas'
+    expect(page).to have_content 'Coca-cola'
+    expect(page).to have_content drink1.description
+    expect(page).to have_content 'Mistério'
+    expect(page).to have_content drink2.description
+    expect(page).to have_content "contém álcool"
+  end
+
+  it 'e não há bebidas cadastradas' do
+    owner = Owner.create!(cpf: '34423090007', name: 'Paula', surname: 'Groselha', email: 'paula@email.com',
+                              password: '123456789012')
+    owner.create_restaurant!(brand_name: 'A Figueira Rubista', corporate_name: 'Figueira Rubista LTDA',
+                            cnpj: '25401196000157', full_address: 'Rua das Flores, 10', phone: '1525017617',
+                            email: 'afigueira@email.com')
+
+    login_as(owner)
+    visit root_path
+
+    expect(page).to have_content 'Ainda não há bebidas cadastradas'
+  end
 end
