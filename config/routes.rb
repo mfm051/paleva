17d1d38eae00f52923ac21d268333058aae005f2
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2024-10-27 23:24:52 -0300

    Adiciona partials para prato e bebida

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root 'restaurants#show'
+
   devise_for :owners, controllers: {registrations: "registrations"}
+
   resource :restaurant, only: [:new, :create]
   resolve('Restaurant') { [:restaurant] }
+
   get '/search', to: "restaurants#search"
+
   resources :dishes, only: [:new, :create, :edit, :update, :destroy]
   resources :drinks, only: [:new, :create, :edit, :update, :destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -15,7 +18,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
